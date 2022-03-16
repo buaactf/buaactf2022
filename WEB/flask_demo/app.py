11e6d565db3224610969d568d4de0d5693e65351
FYHSSGSS <fuyunhao3@126.com>
2022-03-16 15:43:09 +0800

    temp_add_web

---
+++ WEB/flask_demo/app.py
@@ -0,0 +1,251 @@
+from io import BytesIO
+
+from flask import g, request, Flask, current_app, jsonify, render_template
+import jwt
+from jwt import exceptions
+import functools
+import datetime
+import random
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from io import BytesIO
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+headers = {
+    'typ': 'jwt',
+    'alg': 'HS256'
+}
+
+# 密钥
+SALT = 'iv%i6xo7l8_t9bf_u!8#g#m*)*+ej@bek6)(@u3kh*42+unjv='
+
+_letter_cases = "abcdefghjkmnpqrstuvwxy"  # 小写字母，去除可能干扰的i，l，o，z
+_upper_cases = _letter_cases.upper()  # 大写字母
+_numbers = ''.join(map(str, range(10)))  # 数字
+init_chars = ''.join((_letter_cases, _upper_cases, _numbers))
+
+
+def create_validate_code(size=(120, 30),
+                         chars=init_chars,
+                         img_type="GIF",
+                         mode="RGB",
+                         bg_color=(230, 230, 230),
+                         fg_color=(18, 18, 18),
+                         font_size=20,
+                         font_type='/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf',
+                         length=4,
+                         draw_lines=True,
+                         n_line=(1, 2),
+                         draw_points=True,
+                         point_chance=1):
+    '''
+    @todo: 生成验证码图片
+    @param size: 图片的大小，格式（宽，高），默认为(120, 30)
+    @param chars: 允许的字符集合，格式字符串
+    @param img_type: 图片保存的格式，默认为GIF，可选的为GIF，JPEG，TIFF，PNG
+    @param mode: 图片模式，默认为RGB
+    @param bg_color: 背景颜色，默认为白色
+    @param fg_color: 前景色，验证码字符颜色，默认为蓝色#0000FF
+    @param font_size: 验证码字体大小
+    @param font_type: 验证码字体的详细路径，默认为 ae_AlArabiya.ttf
+    @param length: 验证码字符个数
+    @param draw_lines: 是否划干扰线
+    @param n_lines: 干扰线的条数范围，格式元组，默认为(1, 2)，只有draw_lines为True时有效
+    @param draw_points: 是否画干扰点
+    @param point_chance: 干扰点出现的概率，大小范围[0, 100]
+    @return: [0]: PIL Image实例
+    @return: [1]: 验证码图片中的字符串
+    '''
+
+    width, height = size  # 宽， 高
+    img = Image.new(mode, size, bg_color)  # 创建图形
+    draw = ImageDraw.Draw(img)  # 创建画笔
+
+    def get_chars():
+        '''生成给定长度的字符串，返回列表格式'''
+        return random.sample(chars, length)
+
+    def create_lines():
+        '''绘制干扰线'''
+        line_num = random.randint(*n_line)  # 干扰线条数
+
+        for i in range(line_num):
+            # 起始点
+            begin = (random.randint(0, size[0]), random.randint(0, size[1]))
+            # 结束点
+            end = (random.randint(0, size[0]), random.randint(0, size[1]))
+            draw.line([begin, end], fill=(0, 0, 0))
+
+    def create_points():
+        '''绘制干扰点'''
+        chance = min(100, max(0, int(point_chance)))  # 大小限制在[0, 100]
+
+        for w in range(width):
+            for h in range(height):
+                tmp = random.randint(0, 100)
+                if tmp > 100 - chance:
+                    draw.point((w, h), fill=(0, 0, 0))
+
+    def create_strs():
+        '''绘制验证码字符'''
+        c_chars = get_chars()
+        strs = ' %s ' % ' '.join(c_chars)  # 每个字符前后以空格隔开
+
+        font = ImageFont.truetype(font_type, font_size)
+        font_width, font_height = font.getsize(strs)
+
+        draw.text(((width - font_width) / 3, (height - font_height) / 3),
+                  strs, font=font, fill=fg_color)
+        print(c_chars)
+        return ''.join(c_chars)
+
+    if draw_lines:
+        create_lines()
+    if draw_points:
+        create_points()
+    strs = create_strs()
+
+    # 图形扭曲参数
+    params = [1 - float(random.randint(1, 2)) / 100,
+              0,
+              0,
+              0,
+              1 - float(random.randint(1, 10)) / 100,
+              float(random.randint(1, 2)) / 500,
+              0.001,
+              float(random.randint(1, 2)) / 500
+              ]
+    img = img.transform(size, Image.PERSPECTIVE, params)  # 创建扭曲
+    img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)  # 滤镜，边界加强（阈值更大）
+    return img, strs
+
+
+def create_token(username, code):
+    # 构造payload
+    payload = {
+        'username': username,
+        'code': code,  # 自定义用户ID
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)  # 超时时间
+    }
+    result = jwt.encode(payload=payload, key=SALT, algorithm="HS256", headers=headers)
+    return result
+
+
+def verify_jwt(token, secret=None):
+    """
+    检验jwt
+    :param token: jwt
+    :param secret: 密钥
+    :return: dict: payload
+    """
+    if not secret:
+        secret = current_app.config['JWT_SECRET']
+
+    try:
+        payload = jwt.decode(token, secret, algorithms=['HS256'])
+        return payload
+    except exceptions.ExpiredSignatureError:  # 'token已失效'
+        return 1
+    except jwt.DecodeError:  # 'token认证失败'
+        return 2
+    except jwt.InvalidTokenError:  # '非法的token'
+        return 3
+
+
+def login_required(f):
+    '让装饰器装饰的函数属性不会变 -- name属性'
+    '第1种方法,使用functools模块的wraps装饰内部函数'
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            if g.username == 1:
+                return {'code': 4001, 'message': 'token已失效'}, 401
+            elif g.username == 2:
+                return {'code': 4001, 'message': 'token认证失败'}, 401
+            elif g.username == 2:
+                return {'code': 4001, 'message': '非法的token'}, 401
+            else:
+                return f(*args, **kwargs)
+        except BaseException as e:
+            return {'code': 4001, 'message': '请先登录认证.'}, 401
+
+    '第2种方法,在返回内部函数之前,先修改wrapper的name属性'
+    # wrapper.__name__ = f.__name__
+    return wrapper
+
+
+@app.before_request
+def jwt_authentication():
+    """
+    1.获取请求头Authorization中的token
+    2.判断是否以 Bearer开头
+    3.使用jwt模块进行校验
+    4.判断校验结果,成功就提取token中的载荷信息,赋值给g对象保存
+    """
+    token = request.cookies.get('token')
+    if token:
+        "提取token 0-6 被Bearer和空格占用 取下标7以后的所有字符"
+        g.username = None
+        try:
+            "判断token的校验结果"
+            payload = jwt.decode(token, SALT, algorithms=['HS256'])
+            "获取载荷中的信息赋值给g对象"
+            g.username = payload.get('username')
+        except exceptions.ExpiredSignatureError:  # 'token已失效'
+            g.username = 1
+        except jwt.DecodeError:  # 'token认证失败'
+            g.username = 2
+        except jwt.InvalidTokenError:  # '非法的token'
+            g.username = 3
+
+
+@app.route('/code')
+@login_required
+def get_code():
+    # 把strs发给前端,或者在后台使用session保存
+    username = g.username
+    code_img, strs = create_validate_code()
+    buf = BytesIO()
+    code_img.save(buf, 'jpeg')
+
+    buf_str = buf.getvalue()
+    response = app.make_response(buf_str)
+    response.headers['Content-Type'] = 'image/gif'
+    response.set_cookie("token", create_token(username, strs))
+    # return {"code": 200, "message": "success", "Authorization": token}
+    # session['img'] = strs.upper()
+
+    return response
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.form
+            # request.form.get('img')
+        username = data.get("username")
+        password = data.get("password")
+            # 验证账号密码，正确则返回token，用于后续接口权限验证
+        token = create_token(username, '****')
+        # rsp.headers['Authorization'] = 'test_token'
+        return {"code": 200, "message": "success", "Authorization": token}
+
+@app.route('/pdd', methods=['GET', 'POST'])
+@login_required
+def game():
+    username = g.username
+    score = 0
+    if score == 1000:
+        pass
+    return render_template("game.html", score = score)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8090, debug=True)
